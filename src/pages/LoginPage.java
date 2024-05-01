7980fc542357c3c23588baddbebf2abc89a50c68
Jitesh <jiteshdeshmukh0590@gmail.com>
2024-05-01 15:18:01 +0530

    Commited by Git

---
+++ src/pages/LoginPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+public class LoginPage {
+
+public void getTitleOfPage()
+{
+	System.out.println("Fetching Title of Page");
+}
+ public void loginToApp()
+ {
+	 System.out.println("logging to Application");
+	 
+ }
+ 
+ 
+
+
+}
